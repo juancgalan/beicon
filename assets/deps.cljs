25d0fff8af1715ea68ebb6557aee67ab7d037264
Andrey Antukh <niwi@niwi.nz>
2018-12-15 19:15:58 +0100

    Add npm deps into assets/deps.cljs file.

--- assets/deps.cljs
+++ assets/deps.cljs
@@ -1,5 +1,6 @@
-{:foreign-libs
- [{:file "rxjs/rx.js"
-   :file-min "rxjs/rx.min.js"
-   :provides ["beicon.impl.rxjs"]}]
- :externs ["rxjs/externs.js"]}
+{:npm-deps {"rxjs" "5.5.2"}
+ :install-deps true
+ :externs ["rxjs/externs.js"]
+ :foreign-libs [{:file "rxjs/rx.js"
+                 :file-min "rxjs/rx.min.js"
+                 :provides ["beicon.impl.rxjs"]}]}
